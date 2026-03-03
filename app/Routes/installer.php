0ec1b239891da75f686848f5e81aebea92da3fca
0wum0 <florian0engelhardt@gmail.com>
2026-03-03 18:37:53 +0100

    Add explicit route for installer step 4

--- app/Routes/installer.php
+++ app/Routes/installer.php
@@ -9,6 +9,7 @@ use App\Controllers\InstallerController;
 $router->get('/', [InstallerController::class, 'index']);
 $router->get('/install', [InstallerController::class, 'index']);
 $router->get('/install/schritt/{step}', [InstallerController::class, 'index']);
+$router->get('/install/schritt/4', [InstallerController::class, 'index']);
 $router->post('/install/check-db', [InstallerController::class, 'checkDb']);
 $router->post('/install/schritt/2', [InstallerController::class, 'runStep2']);
 $router->post('/install/schritt/3', [InstallerController::class, 'runStep3']);
